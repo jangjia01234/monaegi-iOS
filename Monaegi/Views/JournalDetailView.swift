ef23c68793a406278f8eff5645dcb0507cb770f1
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-04-17 15:53:02 +0900

    :construction: 일기 수정 기능 구현중

--- Monaegi/Views/JournalDetailView.swift
+++ Monaegi/Views/JournalDetailView.swift
@@ -3,80 +3,55 @@ import SwiftUI
 struct JournalDetailView: View {
     var journal: JournalData
     
-    @State private var isEditing: Bool = false
-    
     @State private var title = ""
     @State private var content = ""
     
     @Environment(\.editMode) private var editMode
-    @State private var disableTextEditor = true
-    
+    //    @State private var disableTextEditor = true
     
     var body: some View {
         VStack {
-            HStack {
-                Spacer()
-                
-                Button(action: {
-                    isEditing.toggle()
-                }, label: {
-                    Text("수정")
-                        .fontWeight(.semibold)
-                })
-                .padding(.bottom, 10)
-            }
-            
             VStack(alignment: .leading) {
                 Text(journal.date)
                     .foregroundColor(.gray)
                     .padding(.bottom, 10)
                 
-                TextEditor(text: .constant(self.journal.title))
-                    .frame(height: 40)
-                    .disabled(disableTextEditor)
-                    .onChange(of: editMode?.wrappedValue) { _, newValue in
-                        if (newValue != nil) && (newValue!.isEditing) {
-                            disableTextEditor = false
-                        } else {
-                            disableTextEditor = true
-                        }
-                    }
+                if editMode?.wrappedValue.isEditing == true {
+                    TextField("", text: .constant(self.journal.title))
+                    Divider()
+                    TextField("", text: .constant(self.journal.content))
+                } else {
+                    Text(journal.title)
+                    Divider()
+                    Text(journal.content)
+                }
                 
-                Divider()
-                
-                TextEditor(text: .constant(self.journal.content))
-                    .font(.title3)
+                //                TextEditor(text: .constant(self.journal.title))
+                //                    .frame(height: 40)
+                //                    .disabled(disableTextEditor)
+                //                    .onChange(of: editMode?.wrappedValue) { _, newValue in
+                //                        if (newValue != nil) && (newValue!.isEditing) {
+                //                            disableTextEditor = false
+                //                        } else {
+                //                            disableTextEditor = true
+                //                        }
+                //                    }
+                //
+                //                Divider()
+                //
+                //                TextEditor(text: .constant(self.journal.content))
+                //                    .font(.title3)
             }
-            
-//            if !isEditing {
-//                VStack(alignment: .leading) {
-//                    Text(journal.date)
-//                        .foregroundColor(.gray)
-//                        .padding(.bottom, 10)
-//                    
-//                    TextEditor(text: .constant(self.journal.title))
-//                        .frame(height: 40)
-//                    
-//                    Divider()
-//                    
-//                    TextEditor(text: .constant(self.journal.content))
-//                        .font(.title3)
-//                }
-//            } else {
-//                    TextEditor(text: $title)
-//                        .frame(height: 40)
-//                    
-//                    Divider()
-//                    
-//                    TextEditor(text: $content)
-//                        .font(.subheadline)
-//            }
-            
             Spacer()
+        }
+        .animation(nil, value: editMode?.wrappedValue)
+        .toolbar {
+            EditButton()
         }
         .padding()
     }
 }
+
 
 #Preview {
     JournalDetailView(journal: (JournalData(title: "", content: "", date: "")))
